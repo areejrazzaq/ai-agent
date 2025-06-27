5588179c3371011e916a33e4e06103af1357455b
areejrazzaq <areejrazzaq00@gmail.com>
2025-06-27 16:02:31 +0500

    deploy

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from ai-agent-basic!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from ai_agent import fetch_response_from_ai
 
+class RequestState(BaseModel):
+    llm_id:str
+    user_query:List[str]
+    allow_search:bool
+
+app = FastAPI()
+
+ALLOWED_MODELS = ["qwen-qwq-32b", "llama-3.3-70b-versatile"];
+
+@app.post('/chat')
+def get_response(request: RequestState):
+    """
+    API Endpoint to interact with the Chatbot using LangGraph and search tools.
+    It dynamically selects the model specified in the request
+    """
+    # return error if not in allowed models;
+    if (request.llm_id not in ALLOWED_MODELS):
+        return {"error": "Invalid model name. Kindly select a valid AI model"}
+
+    # Create AI Agent and get response from it! 
+    response= fetch_response_from_ai(request.user_query, request.allow_search, request.llm_id)
+    return response
 
 if __name__ == "__main__":
-    main()
+    # use unicorn to run 
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
